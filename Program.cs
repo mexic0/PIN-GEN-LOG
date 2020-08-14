6ee62b145434955aa2764b42f579ea4b41a4afbb
meixco <69695776+mexic0@users.noreply.github.com>
2020-08-14 22:22:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_Random_PIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generating a pin in the console
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(1000, 9999));
+
+            
+
+
+
+            //PIN Login
+            int pin = rnd.Next(1000, 9999);
+            //pin.Send on Discord / Database..
+
+            Console.WriteLine("");
+            Console.Write("Enter your pin: ");
+            string result = Console.ReadLine();
+
+            if (int.Parse(result) == pin)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Logged !");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("False pin !");
+            }
+            
+            
+
+        }
+    }
+}
